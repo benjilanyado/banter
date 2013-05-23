84c47d665247887b518ac103285dd0d35385e6af
riklomas <lomas.rik@gmail.com>
2013-05-23 14:16:57 +0100

    set up rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Banter::Application.config.secret_token = '273866b77625d0b47edd49d4635fcd3509687859f8b6e6d8234cf29cb4efdfef8048f1d32a8c20261a2ad9156e5e5f69ccf6f1818cd0334ebc86fe6a744cdd70'
